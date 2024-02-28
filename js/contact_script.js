5e204f659e171f19de26027fb2583ce7f5dffeef
Ankush <ankushkumar1021g@gmail.com>
2024-02-28 15:34:40 +0530

    new change

--- js/contact_script.js
+++ js/contact_script.js
@@ -1,9 +1,10 @@
 // Replace with your Airtable API key and base ID
+// const token = 'pat6o2egKcMWacd12.4f0c0d68d40eed719003444f614674b6f7f6a318a860bbf91ff309bcab049de9'
 const apiKey = 'keyrKtxdEwiOsQckT';
 const baseId = 'appBaY4AlC6Xy26g6';
 // patrsRsGwsUkCSM4Y.f2749999b8f206e367f81ce59ecf47fafd2ecfaa650019ae3674c1772461f826
 //patrsRsGwsUkCSM4Y
-document.getElementById('contact_message').addEventListener('submit', function(event) {
+document.getElementById('Contact_message').addEventListener('submit', function(event) {
   event.preventDefault();
 
   const name = document.getElementById('name').value;
@@ -21,7 +22,7 @@ document.getElementById('contact_message').addEventListener('submit', function(e
     }
   };
 
-  fetch(`https://api.airtable.com/v0/${baseId}/Contact_message`, {
+  fetch(`https://api.airtable.com/v0/${baseId}/Contact`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
